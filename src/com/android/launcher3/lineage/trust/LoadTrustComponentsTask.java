ProjectEverest/android_packages_apps_Launcher3

Launcher3: Rebrand as Everest Launcher

src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
@@ -26,7 +26,7 @@ import android.os.Build;
 
 import androidx.annotation.NonNull;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.everest.EverestUtils;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
 import java.util.ArrayList;
@@ -65,7 +65,7 @@ public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<Trust
                     PackageInfoFlags.of(Long.valueOf(PackageManager.MATCH_ALL)));
 
         int numPackages = apps.size();
-        List<String> launchablePackages = Utils.launchablePackages(mContext);
+        List<String> launchablePackages = EverestUtils.launchablePackages(mContext);
         List<String> whiteListedPackages = Arrays.asList(mContext.getResources().getStringArray(
                 com.android.internal.R.array.config_appLockAllowedSystemApps));
 

src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
@@ -33,7 +33,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.everest.EverestUtils;
 import com.android.launcher3.R;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
@@ -108,7 +108,7 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
 
             mProtectedView.setVisibility(hasSecureKeyguard ? View.VISIBLE : View.GONE);
 
-            mHiddenView.setVisibility(Utils.launchablePackages(mContext).contains(component.getPackageName()) ?
+            mHiddenView.setVisibility(EverestUtils.launchablePackages(mContext).contains(component.getPackageName()) ?
                     View.VISIBLE : View.GONE);
 
             mHiddenView.setOnClickListener(v -> {

crazy-stuffs/packages_apps_Launcher3

src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
@@ -26,7 +26,7 @@ import android.os.Build;
 
 import androidx.annotation.NonNull;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.everest.Utils;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
 import java.util.ArrayList;

src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
@@ -33,7 +33,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.everest.Utils;
 import com.android.launcher3.R;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 